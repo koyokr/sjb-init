0b16a2cd4833743d0c9a064b7c01d4cc29abafeb
koyo <koyo@koyo.kr>
2018-08-17 11:23:47 +0900

    Set encoding of locale.json to ASCII

--- install-vscode.ps1
+++ install-vscode.ps1
@@ -8,7 +8,7 @@ $env:Path = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTa
 
 # install extension language-pack-ko
 code --install-extension ms-ceintl.vscode-language-pack-ko
-'{"locale":"ko"}' | Set-Content -Encoding UTF8 -Path $env:APPDATA\Code\User\locale.json
+'{"locale":"ko"}' | Set-Content -Path $env:APPDATA\Code\User\locale.json
 
 # install extension other
 code --install-extension eamodio.gitlens
