d2eaad1d94b8551746e7b2c9e203bcfb82000257
koyo <koyo@koyo.kr>
2018-08-17 14:54:21 +0900

    Change condition from NOT =="" to NOT DEFINED

--- init.bat
+++ init.bat
@@ -4,11 +4,10 @@
 SET POSH=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass
 
 :: install choco
-IF NOT %ChocolateyInstall%=="" GOTO INSTALLED
-%POSH% -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
-SET Path=%Path%;%ALLUSERSPROFILE%\chocolatey\bin
-
-:INSTALLED
+IF NOT DEFINED %ChocolateyInstall% (
+  %POSH% -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+  SET Path=%Path%;%ALLUSERSPROFILE%\chocolatey\bin
+)
 :: install curl
 choco install curl -y
 
