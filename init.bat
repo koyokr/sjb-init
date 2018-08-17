a4807abd4f85dc25398a7e64101fb8262b62f273
koyo <koyo@koyo.kr>
2018-08-17 09:28:55 +0900

    Update init.bat

--- init.bat
+++ init.bat
@@ -4,10 +4,11 @@
 SET POSH=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass
 
 :: install choco
-IF %ChocolateyInstall%=="" (
-  %POSH% -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
-  SET Path=%Path%;%ALLUSERSPROFILE%\chocolatey\bin
-)
+IF NOT %ChocolateyInstall%=="" GOTO INSTALLED
+%POSH% -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+SET Path=%Path%;%ALLUSERSPROFILE%\chocolatey\bin
+
+:INSTALLED
 :: install curl
 choco install curl -y
 
