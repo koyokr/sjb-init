a1a53b0ec0c10351a1fd07cd48eb02c610c19f89
koyo <koyo@koyo.kr>
2018-08-27 16:22:51 +0900

    Pin firefox to taskbar

--- static/src/install-firefox.ps1
+++ static/src/install-firefox.ps1
@@ -2,3 +2,7 @@
 
 # install firefox
 choco install firefox -y
+
+# https://stackoverflow.com/questions/9739772/how-to-pin-to-taskbar-using-powershell
+$ff = (New-Object -ComObject Shell.Application).NameSpace("$env:ProgramFiles\Mozilla Firefox").ParseName('firefox.exe')
+$ff.InvokeVerb('taskbarpin')
