0bd626f90e83ea47e5ca09ff64dfefba759fe4e1
koyo <koyo@koyo.kr>
2018-08-29 04:55:58 +0000

    Deploy koyokr/sjb-init to github.com/koyokr/sjb-init.git:gh-pages

--- src/install-vscode.ps1
+++ src/install-vscode.ps1
@@ -6,9 +6,25 @@ refresh-env 'Path'
 
 # install extension language-pack-ko
 code --install-extension ms-ceintl.vscode-language-pack-ko
-'{"locale":"ko"}' | Set-Content -Path $env:APPDATA\Code\User\locale.json
+@'
+{
+    "locale": "ko"
+}
+'@ | Set-Content -Path $env:APPDATA\Code\User\locale.json
 
 # install extension other
 code --install-extension eamodio.gitlens
 code --install-extension avli.clojure
 code --install-extension ms-vscode.csharp
+@'
+[
+    {
+        "key": "ctrl+oem_period",
+        "command": "clojureVSCode.eval"
+    },
+    {
+        "key": "ctrl+oem_2",
+        "command": "clojureVSCode.evalAndShowResult"
+    }
+]
+'@ | Set-Content -Path $env:APPDATA\Code\User\keybindings.json
