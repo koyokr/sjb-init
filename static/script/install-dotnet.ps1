da1198161bf286a6f48b9b24590b06488ea3f5a0
koyo <koyo@koyo.kr>
2018-09-04 10:55:29 +0900

    install-dotnet: Modify netfx version, Add extension

--- static/script/install-dotnet.ps1
+++ static/script/install-dotnet.ps1
@@ -1,7 +1,8 @@
 # require choco, vscode
 
 # install dotnet, netfx
-choco install dotnetcore-sdk netfx-4.7.2-devpack -y
+choco install dotnetcore-sdk netfx-4.6.2-devpack -y
 
 # install vscode extension
 code --install-extension ms-vscode.csharp
+code --install-extension jchannon.csharpextensions
