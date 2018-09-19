3ca4c959c97aac3f58fac5a0b1da695854407dd8
koyo <koyo@koyo.kr>
2018-09-19 07:06:08 +0000

    Deploy koyokr/sjb-init to github.com/koyokr/sjb-init.git:gh-pages

--- script/install-packages.ps1
+++ script/install-packages.ps1
@@ -3,5 +3,14 @@
 # install googlechrome
 choco install googlechrome -y
 
+# set chrome as default browser
+$regKey = 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\{0}\UserChoice'
+$regKeyFtp   = $regKey -f 'ftp'
+$regKeyHttp  = $regKey -f 'http'
+$regKeyHttps = $regKey -f 'https'
+Set-ItemProperty $regKeyFtp   -Name ProgId ChromeHTML
+Set-ItemProperty $regKeyHttp  -Name ProgId ChromeHTML
+Set-ItemProperty $regKeyHttps -Name ProgId ChromeHTML
+
 # install 7zip
 choco install 7zip.install -y
